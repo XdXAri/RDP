9a7c58863262d8b74624c7a8eb29214579adbd07
hicham0919 <65982229+hicham0919@users.noreply.github.com>
2021-03-27 21:09:26 +0100

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -6,7 +6,7 @@ sudo adduser HCHAM sudo
 echo 'HCHAM:1234' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
-sudo apt-get uprade
+
 
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
