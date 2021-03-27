85b8874bfdffaccbcd2455b23e3c96a2637dc6b0
hicham0919 <65982229+hicham0919@users.noreply.github.com>
2021-03-27 22:17:24 +0100

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -5,21 +5,20 @@ sudo useradd -m HCHAM
 sudo adduser HCHAM sudo
 echo 'HCHAM:1234' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
-sudo apt-get update
+
 
 
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
-sudo apt install --assume-yes --fix-broken
+
 sudo DEBIAN_FRONTEND=noninteractive \
 
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
-sudo apt install --assume-yes xscreensaver
+
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
-sudo apt install --assume-yes --fix-broken
-sudo apt install nautilus nano -y 
+
 
 
 
