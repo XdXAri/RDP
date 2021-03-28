ea05f348545cb787a86e9475a171399e7032cb71
hicham0919 <65982229+hicham0919@users.noreply.github.com>
2021-03-28 11:44:32 +0100

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -5,33 +5,24 @@ sudo useradd -m HCHAM
 sudo adduser HCHAM sudo
 echo 'HCHAM:1234' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
-
-
-
+sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
-
+sudo apt install --assume-yes --fix-broken
 sudo DEBIAN_FRONTEND=noninteractive \
-
+apt install --assume-yes xfce4 desktop-base
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
-
-
+sudo apt install --assume-yes xscreensaver
+sudo systemctl disable lightdm.service
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
-
-
-
-
-
-
+sudo apt install --assume-yes --fix-broken
 sudo adduser HCHAM chrome-remote-desktop
-
-
 } &> /dev/null &&
 printf "\nSetup Completed " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - HCHAM -c """$CRP"""
+su - HCHAM1 -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n'
 printf 'Your SUDO Pasword Is 1234 \n'
