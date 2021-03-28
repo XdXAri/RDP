a898ecc792ef6920ca04c468e82e816ee5dc09b3
hicham0919 <65982229+hicham0919@users.noreply.github.com>
2021-03-28 11:57:11 +0100

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -17,12 +17,17 @@ sudo systemctl disable lightdm.service
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
+sudo apt install nautilus nano -y 
+sudo apt -y install obs-studio
+sudo apt -y install firefox
+sudo apt -y install qbittorrent
+sudo apt install nload
 sudo adduser HCHAM chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Completed " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - HCHAM1 -c """$CRP"""
+su - HCHAM -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n'
 printf 'Your SUDO Pasword Is 1234 \n'
